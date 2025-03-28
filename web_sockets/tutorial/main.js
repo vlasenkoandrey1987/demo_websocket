7dbfee63237b73c3fd3019f39eeb7b7f56e1ccc6
Andrey Vlasenko <andrey.vlasenko@evraz.com>
2025-03-28 16:08:44 +0300

    Add websockets tutorial: Part 2 - Route & broadcast

--- web_sockets/tutorial/main.js
+++ web_sockets/tutorial/main.js
@@ -1,5 +1,23 @@
 import { createBoard, playMove } from "./connect4.js";
 
+function initGame(websocket) {
+  websocket.addEventListener("open", () => {
+    // Send an "init" event according to who is connecting.
+    const params = new URLSearchParams(window.location.search);
+    let event = { type: "init" };
+    if (params.has("join")) {
+      // Second player joins an existing game.
+      event.join = params.get("join");
+    } else if (params.has("watch")) {
+      // Spectator watches an existing game.
+      event.watch = params.get("watch");
+    } else {
+      // First player starts a new game.
+    }
+    websocket.send(JSON.stringify(event));
+  });
+}
+
 function showMessage(message) {
   window.setTimeout(() => window.alert(message), 50);
 }
@@ -8,6 +26,11 @@ function receiveMoves(board, websocket) {
   websocket.addEventListener("message", ({ data }) => {
     const event = JSON.parse(data);
     switch (event.type) {
+      case "init":
+        // Create links for inviting the second player and spectators.
+        document.querySelector(".join").href = "?join=" + event.join;
+        document.querySelector(".watch").href = "?watch=" + event.watch;
+        break;
       case "play":
         // Update the UI with the move.
         playMove(board, event.player, event.column, event.row);
@@ -27,6 +50,12 @@ function receiveMoves(board, websocket) {
 }
 
 function sendMoves(board, websocket) {
+  // Don't send moves for a spectator watching a game.
+  const params = new URLSearchParams(window.location.search);
+  if (params.has("watch")) {
+    return;
+  }
+
   // When clicking a column, send a "play" event for a move in that column.
   board.addEventListener("click", ({ target }) => {
     const column = target.dataset.column;
@@ -48,6 +77,7 @@ window.addEventListener("DOMContentLoaded", () => {
   createBoard(board);
   // Open the WebSocket connection and register event handlers.
   const websocket = new WebSocket("ws://localhost:8001/");
+  initGame(websocket);
   receiveMoves(board, websocket);
   sendMoves(board, websocket);
 });
